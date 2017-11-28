6e0fa51caf68a743816725cb09f86a829cca0493
viirii <christinebaek@gmail.com>
2017-11-28 15:31:56 -0800

    remapping of quit keys for emacsdaemon

--- init.el
+++ init.el
@@ -78,4 +78,5 @@
 (when (file-exists-p user-settings-dir)
   (mapc 'load (directory-files user-settings-dir nil "^[^#].*el$")))
 
-;;(windmove-default-keybindings)
+(global-set-key "\C-z"     'undo)
+(global-set-key "\C-x\C-c" 'undo)

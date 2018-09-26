73b730f1caa88e93a909dbfd936d57c55ea07e37
Christine Baek <christine.baek@mixpanel.com>
2018-09-26 21:20:31 +0000

    removed elpa stuff.

--- elpa/bind-key-20161218.1520/bind-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "bind-key" "20161218.1520" "A simple way to manage personal keybindings" 'nil :commit "6a9c8852d3c3747a01ed1f118434c3bfd686e481" :url "https://github.com/jwiegley/use-package" :keywords '("keys" "keybinding" "config" "dotemacs"))
--- elpa/swiper-20171124.804/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20171124.804" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "1240d882355291b445ae7f2499f114f3e5c079ac" :url "https://github.com/abo-abo/swiper" :keywords '("matching"))

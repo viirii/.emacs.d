82cc3a231d9866f97733556451b7446da6c0d379
Christine Baek <christine.baek@mixpanel.com>
2019-02-01 23:25:22 +0000

    elpas too

---
+++ elpa/clojure-mode-extra-font-locking-20171102.1020/clojure-mode-extra-font-locking-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "clojure-mode-extra-font-locking" "20171102.1020" "Extra font-locking for Clojure mode" '((clojure-mode "3.0")) :commit "118c19700c904ae6a45fb409ca795bb93ff8dbd8" :url "http://github.com/clojure-emacs/clojure-mode" :keywords '("languages" "lisp"))
---
+++ elpa/swiper-20171124.804/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20171124.804" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "1240d882355291b445ae7f2499f114f3e5c079ac" :url "https://github.com/abo-abo/swiper" :keywords '("matching"))

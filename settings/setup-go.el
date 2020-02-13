df83745d6e926e1d847fc1fcf2f327ed6fda6d4e
Christine Baek <christinebaek@gmail.com>
2020-02-13 23:55:09 +0000

    rename stuff

--- settings/setup-go.el
+++ settings/setup-go.el
@@ -32,10 +32,10 @@
   ; Godef jump key binding
   (local-set-key (kbd "M-.") 'godef-jump)
   (local-set-key (kbd "M-*") 'pop-tag-mark)
-  (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
-  (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
-  (local-set-key (kbd "M-]") 'next-error)         ; Go to next error (or msg)
-  (local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
+;;  (local-set-key (kbd "M-p") 'compile)            ; Invoke compiler
+;;  (local-set-key (kbd "M-P") 'recompile)          ; Redo most recent compile cmd
+;;  (local-set-key (kbd "M-]") 'next-error)         ; Go to next error (or msg)
+;;  (local-set-key (kbd "M-[") 'previous-error)     ; Go to previous error or msg
 )
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
